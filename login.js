45ec54fc350da421717985350bbc22a9d34aaf3b
rvasquez <rvasquez1@gmail.com>
2021-01-08 12:53:22 -0500

    adding comments, validation and local storage

--- login.js
+++ login.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-
+//global variables and getting values from local storage
     var userNameSavedString = localStorage.getItem("userNameSaved");
     var userNameSaved = JSON.parse(userNameSavedString);
     if (userNameSaved == null) {
         userNameSaved = {};
       }
-
+//submit button
     $("#submitBtn").on("click", function(event) {
         event.preventDefault();
         $("#msgError").text("");
@@ -32,10 +32,11 @@ $(document).ready(function () {
 
 function comparison(userName, userPass){
     console.log(userName +" "+ userPass + " " + userNameSaved[userName]);
-    
+    //get all the usernames to check if it already exist
     var keys = Object.keys(userNameSaved)
     if (keys.indexOf(userName) > -1) {
         console.log("user already");
+        //if the user is already in local storage
         if (userPass == userNameSaved[userName]){
             console.log("im here");
             alert("you pass");
