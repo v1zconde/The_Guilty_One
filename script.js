88a39ca91ee53b42851bda61d691fd6eae44a268
rvasquez <rvasquez1@gmail.com>
2021-01-08 01:41:01 -0500

    adding the query for nutritionix working

--- script.js
+++ script.js
@@ -11,3 +11,16 @@
     });
   
 
+    var appKey = "64294de991310089661a16d9cd168ca1"
+    var appId = "8e63a8a3"
+    var query = "Cookies"
+    
+    var queryURL = "https://api.nutritionix.com/v1_1/search/" +query+ "?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId=" + appId + "&appKey=" + appKey
+    
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      
+      console.log(response);
+    });
